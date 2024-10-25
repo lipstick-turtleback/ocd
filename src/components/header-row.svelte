2d3b7928e7d4011efc2b3a2f0339565457fdf9d1
lipstick-turtleback <857949+lipstick-turtleback@users.noreply.github.com>
2024-10-25 14:45:23 +0300

    Update header-row.svelte

--- src/components/header-row.svelte
+++ src/components/header-row.svelte
@@ -4,7 +4,7 @@
 	const textTransformOptions = ['none', 'lowercase', 'uppercase'];
 	let selectedtextTransformOption = $state(0);
 
-	const onTextTransformSwitchClick = async (event) => {
+	const onTextTransformSwitchClick = async () => {
 		selectedtextTransformOption = (selectedtextTransformOption + 1) % 3;
 		const appContainerClass = textTransformOptions[selectedtextTransformOption];
 		console.log(appContainerClass);
@@ -13,9 +13,11 @@
 </script>
 
 <div class="row first-row">
-	<!-- svelte-ignore a11y_click_events_have_key_events -->
-	<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
-	<h1 class="text-transform-switch" onclick={onTextTransformSwitchClick}>Offline СSV Dictionary</h1>
+	<h1 class="text-transform-switch">
+		<!-- svelte-ignore a11y_invalid_attribute -->
+		<a href="#" onclick={onTextTransformSwitchClick}>Offline СSV Dictionary</a>
+	</h1>
+
 	<div class="version-number">
 		<a href="https://github.com/lipstick-turtleback/awesome-dictionary" target="_blank">
 			v24.10.24
@@ -24,8 +26,8 @@
 </div>
 
 <style>
-	.text-transform-switch:hover {
-		text-decoration: underline;
-		cursor: pointer;
+	.text-transform-switch a {
+		text-decoration: none;
+		color: #333;
 	}
 </style>
